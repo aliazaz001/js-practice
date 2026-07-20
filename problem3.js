83284af1b7630138443c1788b6d7ff952250e115
Ali <aliazazrafe2000@gmail.com>
2026-07-20 20:35:08 +0600

    When variable is true

---
+++ problem3.js
@@ -0,0 +1,16 @@
+//problem3.Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+let isRaining =true;
+ if(isRaining){
+    console .log ("Take an umbrella");
+}
+else{
+    console.log("No need for an umbrella");
+ }
+
+// let isRaining=false;
+// if(isRaining){
+//     console .log ("Take an umbrella");
+// }
+// else{
+//     console.log("No need for an umbrella");
+// }
