861fa77ff67006840172d666d88ecbdc3a039821
Ali <aliazazrafe2000@gmail.com>
2026-07-20 19:48:27 +0600

    Add problem1 solution

--- new1.js
+++
@@ -1,3 +0,0 @@
-const a=50;
-const b=50;
-console.log(a+b);
---
+++ problem1.js
@@ -0,0 +1,13 @@
+//Problem 1:Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+//1.1. If price1 and price2 == then it should print true, else false.
+//1.2.comparison between less than or greater than should also be printed with console.log.
+let price1=100;
+let price2=150; 
+ console.log(price1==price2);
+ console.log(price1===price2);
+ console.log(price1!=price2);
+ console.log(price1!==price2);
+ console.log(price1>price2);
+console.log(price1<price2);
+console.log(price1>=price2);
+console.log(price1<=price2);
