9e6bd7f58b8cf4513d28808b4216c9b63a4b0dda
Ali <aliazazrafe2000@gmail.com>
2026-07-24 00:37:43 +0600

    added problem8

---
+++ problem8.js
@@ -0,0 +1,39 @@
+//Age 18 entry ticket
+
+// const age=18;
+// if(age>=18){
+//     console.log("Entry not restrickted");
+// }
+// else{
+//     console.log("Entry restricted");
+// }
+// const age=28;
+// if(age!=28){
+//     console.log("Entry restricted");
+
+// }
+// else{
+//     console.log("Entry not restricted");
+
+// }
+
+// const age=18;
+// if(age===18){
+//     console.log("Entry not restricted")
+// }
+// else{
+//     console.log("Entry restricted");
+// }
+
+const age=18;
+if(age<=18)
+{
+console.log("Entry restricted");
+
+}
+else{
+    console.log("Entry not restricted");
+
+}
+
+
