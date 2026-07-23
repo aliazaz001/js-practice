cc7be26f2de6b0841aca3b11ec70f89ff7f1b72d
Ali <aliazazrafe2000@gmail.com>
2026-07-23 22:01:01 +0600

    Complete problem 3

--- problem3.js
+++ problem3.js
@@ -1,16 +1,15 @@
 //problem3.Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
-let isRaining =true;
- if(isRaining){
-    console .log ("Take an umbrella");
+// let isRaining =true;
+//  if(isRaining){
+//     console .log ("Take an umbrella");
+// }
+// else{
+//     console.log("No need for an umbrella");
+//  }
+let isRaining=false;
+if(isRaining){
+   console.log("Take an umbrella");
 }
 else{
-    console.log("No need for an umbrella");
- }
-
-let isRaining=false;
- if(isRaining){
-    console .log ("Take an umbrella");
- }
-else{
-     console.log("No need for an umbrella");
- }
+   console.log("dont take an umberlla");
+}
