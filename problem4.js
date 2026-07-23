ed9c3dc1b9b3dad62f244a6569f1af5f00f94301
Ali <aliazazrafe2000@gmail.com>
2026-07-23 22:28:19 +0600

    Solve problem 4

---
+++ problem4.js
@@ -0,0 +1,14 @@
+// const stock=0;
+// if(stock===0){
+//     console.log("Out of stock");
+// }
+// else{
+//     console.log("In stock");
+// }
+const stock=1;
+if(stock===1){
+    console.log( "In stock")
+}
+else{
+    console.log("out of stock")
+}
