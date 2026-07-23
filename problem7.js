aa650a99fd8be051f96daa10070cf83b2411b5c5
Ali <aliazazrafe2000@gmail.com>
2026-07-23 23:51:53 +0600

    problem7 added

---
+++ problem7.js
@@ -0,0 +1,24 @@
+// Easy  speed limit problem
+// const speed=80;
+// if (speed<80){
+//     console.log("It is over speed")
+
+// }
+// else{
+//     console.log("it is not overspeed")
+// }
+const speed=80;
+if(speed>80)
+{
+    console.log("speed is in limit");
+}
+else{
+    console.log("Is not in limit")
+}
+
+
+
+
+
+
+
