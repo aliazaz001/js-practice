68b605ffa05837965fc02e60c0ec777c30b24eb1
Ali <aliazazrafe2000@gmail.com>
2026-07-23 22:37:18 +0600

    problem5 added

---
+++ problem5.js
@@ -0,0 +1,9 @@
+//Take a number variable and check whether it is positive or negative (using if-else).
+const number=5;
+if(number=>0){
+    console.log("This is a positive number")
+
+}
+else{
+    console.log("This is a negative number ")
+}
