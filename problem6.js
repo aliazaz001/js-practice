ee67762d1a75957f17fe4dc09ee7e4d3091ad6ce
Ali <aliazazrafe2000@gmail.com>
2026-07-23 23:35:16 +0600

    add problem6

---
+++ problem6.js
@@ -0,0 +1,15 @@
+// calculator leapyear or not
+// const year=2024;
+// if( year%4===0){
+//     console.log("This year is leapyear");
+// }
+// else{
+//     console.log("This year is not a leapyear");
+// }
+const year=2030;
+if(year%4===0){
+    console.log("This year is leapyear");
+}
+else{
+    console.log("This year not leapyear ");
+}
