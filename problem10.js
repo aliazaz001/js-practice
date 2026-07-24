2dbaaef278ff09ae8b6bcf78851bc93f9ddc357c
Ali <aliazazrafe2000@gmail.com>
2026-07-24 13:02:35 +0600

    problem10 added

---
+++ problem10.js
@@ -0,0 +1,10 @@
+//Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+let isWeekend=true;
+let isHoliday=false;
+if(isWeekend||isHoliday){
+    console.log("No work today")
+}
+else{
+    console.log("working day")
+    
+}
