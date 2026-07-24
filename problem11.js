a19d1102d39b53a2be514b1f8a1022b78aee9369
Ali <aliazazrafe2000@gmail.com>
2026-07-24 14:57:01 +0600

    problem 11 added

---
+++ problem11.js
@@ -0,0 +1,10 @@
+//Build a simple login system — print "Login successful" only if both username and password are correct.
+const userName="Admin";
+const password="1234";
+if( userName=="Admin" && password=="1234" )
+    {
+    console.log("Successfull login");
+}
+else{
+    console.log("Wrong password");
+}
