a614c87d4865c12c11ed165ccd2b46d75dae07ad
Ali <aliazazrafe2000@gmail.com>
2026-07-28 17:31:56 +0600

    Problem 13 added

---
+++ problem13.js
@@ -0,0 +1,16 @@
+//Using a bmi variable, determine the category — Underweight, Normal, Overweight, Obese (use if-else if).
+let bmi=27.5;
+if(bmi<18.5){
+    console.log("Under weight");
+}
+else if (bmi<25){
+    console.log("Normal")
+
+}
+else if(bmi<30){
+    console.log("Overweight");
+
+}
+else{
+    console.log("obese");
+}
